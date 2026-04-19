7ee474165b9d175c9dc78a7561e2684e4a4e201c
“chengang” <15736950327@163.com>
2026-04-19 21:34:50 +0800

    测试 git reset --mixed2

--- main.py
+++ main.py
@@ -186,6 +186,7 @@ VALUES (
     return {"message": "Book added successfully"}
 
 print("cs")
+print("cs2")
 
 # @app.post('/books')
 # async def create_book(book: Book, db: AsyncSession = Depends(get_database)):
